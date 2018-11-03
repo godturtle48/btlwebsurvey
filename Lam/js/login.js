95606aac632146c8b9356f0e8797ebfaee694b3f
godturtle48 <beobeo1998@gmail.com>
2018-11-03 17:38:34 +0700

    add login page'

--- Lam/js/login.js
+++ Lam/js/login.js
@@ -11,3 +11,46 @@ bubbleShape = "circle";
 
 drawName(myName, letterColors);
 bounceBubbles();
+
+$('[requiredInput="true"]').blur(function () {
+	debugger;
+	var currentInput = this;
+    var inputcheck = $('[requiredInput="true"]');
+    var value = $(currentInput).val();
+	if (value.trim().length === 0) {
+        $(currentInput).addClass("border-red");       
+        $(currentInput).attr("title", "Trường này không được để trống");
+    } else {
+        $(currentInput).removeClass("border-red");
+        $(currentInput).removeAttr("title");
+    }
+})
+
+$('#submit-button').click(function () {	
+	debugger;
+    $('[requiredInput="true"]').blur()
+    var username = $('#username').val();
+    var password = $('#password').val();
+    if (username == "admin") {
+    	if (password == "admin") {
+    		window.location ="./dashboard.html"
+    	}
+    	else window.alert('Mật khẩu sai');
+    } else window.alert('Người dùng không tồn tại');
+})
+
+function DoKeyup(e, myself, nextcontrolid) {
+	if (window.event) e = window.event; //de chay ca tren IE
+	if (e.keyCode == 13) {
+		document.getElementById(nextcontrolid).focus();
+	}
+}
+
+$('#username').keyup( function(e) {
+	DoKeyup(e, this, 'password');
+})
+
+
+$('#password').keyup( function(e) {
+	DoKeyup(e, this, 'submit-button');
+})

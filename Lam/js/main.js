2d94ffcacc87b03e15fd1a7971f941a5dc67dfdb
godturtle48 <beobeo1998@gmail.com>
2018-11-18 22:33:29 +0700

    update login page

---
+++ Lam/js/main.js
@@ -0,0 +1,5 @@
+$('button').click(function () {
+	var username = $('#username').val();
+	var check = string.lastIndexOf("@vnu.edu.vn");
+	if (check === -1) alert('Mail phải có định dạng @vnu.edu.vn');
+})
